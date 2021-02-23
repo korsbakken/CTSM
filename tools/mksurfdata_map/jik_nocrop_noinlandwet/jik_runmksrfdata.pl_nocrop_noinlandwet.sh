9677c8757c592b5f17e889b1bb02d3f649aadfc3
Jan Ivar Korsbakken <jan.ivar.korsbakken@cicero.oslo.no>
2021-02-23 07:22:54 +0100

    Removed surfdat nc files from tools directory.
    These were large surface data files generated by mksurfdat.pl, and
    scripts/logs related to creating them. These have all been moved to the
    inputdata tree (under cicero_mods).

--- tools/mksurfdata_map/jik_nocrop_noinlandwet/jik_runmksrfdata.pl_nocrop_noinlandwet.sh
+++
@@ -1,2 +0,0 @@
-# Command used to build the surface data set without crop, without inland wetlands
-./mksurfdata.pl -hirespft -years 2005 -no-crop -dinlc /cluster/projects/nn9188k/cesm2.0/inputdata -res usrspec -usr_gname Fennoscandia0.05x0.1 -usr_gdate 201123 -usr_mapdir /cluster/projects/nn9188k/cesm2.0/inputdata/cicero_mods/lnd/clm2/mappingdata/maps/Fennoscandia0.05x0.1 2>&1 | tee jikrun_mksurfdata.pl_$(jiktimestr).log
