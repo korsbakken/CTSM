68fe728ac0fcb2125d88fc2733a2574122ed296f
Jan Ivar Korsbakken <janko@cicero.oslo.no>
2026-01-30 10:06:33 +0100

    Reverting rearrangement of terms in urban lw radiation check.
    That removes explicit floating point overflows (which are perhaps
    "underflows"), but the check still gives obviously faulty numbers and fails.

--- src/biogeophys/UrbanRadiationMod.F90
+++ src/biogeophys/UrbanRadiationMod.F90
@@ -691,7 +691,7 @@ contains
          ! atmospheric longwave radiation to road and walls is conserved as
          ! lwdown (from atmosphere) = lwdown_improad + lwdown_perroad + (lwdown_sunwall + lwdown_shadewall)*canyon_hwr
 
-         err = lwdown(1) + lwnet_canyon(1) - lwup_canyon(1)
+         err = lwnet_canyon(l) - (lwup_canyon(l) - lwdown(l))
          if (abs(err) > .10_r8 ) then
             write (iulog,*) 'urban net longwave radiation balance error',err,' (lwdown ',lwdown(1),', lwup_canyon ',lwup_canyon(1),', lwnet_canyon ',lwnet_canyon(1),')'
             write (iulog,*) 'clm model is stopping'
